fd84479e2f66c9c4674cc7af6164073beb65393c
DapperNurd <jbenbonus@gmail.com>
2019-03-27 00:38:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bot = new Commando.Client({
  
 require('events').EventEmitter.defaultMaxListeners = 20;
  
+var pieCount;
+
 var sendText = "Kim wants you to have a slice of her delicious ";
- 
-var pieCount = 1400;
  
 var commonPies = ["pumpkin pie", "coconut cream pie", "banana cream pie", "strawberry rhubarb pie", "chocolate cream pie", "blueberry pie", "ice cream pie",
     "peach pie", "pear pie", "chicken pot pie", "cranberry pie", "pineapple pie", "turtle pie"
@@ -20,9 +20,17 @@ var uncommonPies = ["apple pie", "cherry pie", "key lime pie", "lemon meringue p
 var rarePies = ["cheesecake", "prickly pear pie", "peach pie à la mode", "apple pie à la mode", "blackberry pie à la mode", "cherry pie à la mode",
     "raspberry pie à la mode", "blueberry pie à la mode"
 ];
- 
+
 var legendaryPies = ["creampie", "cow pie", "cutie pie"];
  
+bot.on('ready', function() {
+    console.log("Ready!");
+    bot.channels.get("560345281577877514").fetchMessages({ limit: 1 }).then(messages => {
+        var lastPieCount = messages.first();
+        pieCount = lastPieCount.content;
+    });
+});
+
 bot.on('message', function(message) {
  
     var sentMessage = message.content.split(" ", 2);
@@ -50,31 +58,24 @@ bot.on('message', function(message) {
             newPie = commonPies[Math.floor(Math.random() * commonPies.length)];
         }
  
-        pieCount++
+            pieCount++
+ 
+        message.channel.send("Here, " + piePerson + "! " + sendText + newPie + "! Kim has given out " + pieCount + " pies on Discord.").then(function (botSentMessage){
+            if(newPie == "prickly pear pie") {
+                botSentMessage.react("🌵");
+            }
+            else if (pieCount.toString().includes("69")) {
+                botSentMessage.react("😏");
+            }
+            else if ((newPie == "pecan pie" && message.author.id.toString() == "307350352594862080") || (newPie == "pecan pie" && (piePerson.toLowerCase() == "kecatas" ||
+            piePerson.toLowerCase() == "kec" || piePerson.toLowerCase() == "cactus" || piePerson.toLowerCase() == "kacatas"))) {
+                botSentMessage.react("😂");
+            }
+        });
 
-        message.channel.sendMessage("Here, " + piePerson + "! " + sendText + newPie + "! Kim has given out " + pieCount + " pies on Discord.").then(function (botSentMessage){
-                            if(newPie == "prickly pear pie") {
-                                botSentMessage.react("🌵");
-                            }
-                            else if (pieCount.toString().includes("69")) {
-                                botSentMessage.react("😏");
-                            }
-                            else if ((newPie == "pecan pie" && message.author.id.toString() == "307350352594862080") || (newPie == "pecan pie" && (piePerson.toLowerCase() == "kecatas" ||
-                            piePerson.toLowerCase() == "kec" || piePerson.toLowerCase() == "cactus" || piePerson.toLowerCase() == "kacatas"))) {
-                                botSentMessage.react("😂");
-                  }
-              });
-        }
-    });
- 
-bot.on('uncaughtException', (err) => {
-    console.error('There was an uncaught error', err);
-    message.channel.sendMessage(err);
-    bot.exit(1); //mandatory (as per the Node docs)
-    });
- 
-bot.on('ready', function() {
-    console.log("Ready!");
+        bot.channels.get("560345281577877514").send(pieCount);
+
+	}
 });
- 
+
 bot.login(process.env.BOT_TOKEN);
