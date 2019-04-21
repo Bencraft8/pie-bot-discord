d65896492f243cd67d245119ebda482d89a84dce
DapperNurd <jbenbonus@gmail.com>
2019-04-21 01:27:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var sendText = "Kim wants you to have a slice of her ";
  
 var commonPies = [" pumpkin pie", " coconut cream pie", " banana cream pie", " strawberry rhubarb pie", " chocolate cream pie", " blueberry pie", " ice cream pie",
     " peach pie", " pear pie", " chicken pot pie", " cranberry pie", " pineapple pie", " turtle pie", " chocolate hazelnut pie", " mixed berry pie", " chestnut pie",
-                  " easter egg pie"
+                  " easter egg pie", " easter egg pie"
 ];
  
 var uncommonPies = [" apple pie", " cherry pie", " key lime pie", " lemon meringue pie", " blackberry pie", " raspberry pie", " pecan pie",
